95e97e7051598a464d16543c08f53ab8c06a4db8
chenyun <hg.c.y.mg14@nycu.edu.tw>
2025-11-24 11:18:20 +0800

    GUI 自動提交

--- src/p2.cpp
+++ src/p2.cpp
@@ -9,6 +9,11 @@ using namespace std;
 //       Recursive case: 回傳 n + sum(n-1)
 int sum(int n) {
     // 在此實作你的程式碼
+    if(n==1){
+        return 1;    
+    }else{
+        return n+sum(n-1);
+    }
     return 0;
 }
 
@@ -16,6 +21,9 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // TODO: 讀取 n 並呼叫 sum 函數，輸出結果
+    int n;
+    cin>>n;
+    cout<<sum(n)<<endl;
     
     return 0;
 }
