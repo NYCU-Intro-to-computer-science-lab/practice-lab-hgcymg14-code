eef2dd78212d60cf4188f38573354c26a639a148
chenyun <hg.c.y.mg14@nycu.edu.tw>
2025-11-24 11:26:22 +0800

    GUI 自動提交

--- src/p4.cpp
+++ src/p4.cpp
@@ -9,6 +9,11 @@ using namespace std;
 //       else return f91(f91(n + 11));
 int f91(int n) {
     // 在此實作你的程式碼
+    if(n>=101){
+        return n-10;
+    }else{
+        return f91(f91(n+11));
+    }
     return 0;
 }
 
@@ -16,6 +21,9 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // TODO: 讀取 n 並呼叫 f91 函數，輸出結果
+    int n;
+    cin>>n;
+    cout<<f91(n)<<endl;
     
     return 0;
 }
